1ebf350f2163153577a253ad0206bc0f63294aba
sinyilin <sinyilin@novastargame.net>
2024-08-21 11:11:47 +0800

    圖像識別使用ios ML 圖像文字識別使用firebase 新增選擇文字識別的語言seg

---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'coreMLTest' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for coreMLTest
+pod 'GoogleMLKit/ImageLabeling', '3.2.0'
+pod 'GoogleMLKit/TextRecognition', '3.2.0'
+pod 'GoogleMLKit/TextRecognitionChinese', '3.2.0'
+pod 'GoogleMLKit/TextRecognitionDevanagari', '3.2.0'
+pod 'GoogleMLKit/TextRecognitionJapanese', '3.2.0'
+pod 'GoogleMLKit/TextRecognitionKorean', '3.2.0'
+end
--- coreMLTest/ViewController.swift
+++ coreMLTest/ViewController.swift
@@ -8,6 +8,14 @@
 import UIKit
 import CoreML
 import Vision
+import MLImage
+import MLKit
+import MLKitTextRecognition
+import MLKitTextRecognitionCommon
+import MLKitTextRecognitionKorean
+import MLKitTextRecognitionChinese
+import MLKitTextRecognitionJapanese
+import MLKitTextRecognitionDevanagari
 
 class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
 
@@ -17,6 +25,7 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
     var cameraBtn:UIButton!
     var mlModel:MobileNetV2!
     let imagePredictor = ImagePredictor()
+    var languageSeg:UISegmentedControl!
     //最大預測數
     let predictionsToShow = 2
     override func viewDidLoad() {
@@ -48,10 +57,11 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
         self.view.backgroundColor = .white
         imgV = UIImageView(frame: .zero)
         imgV.contentMode = .scaleAspectFit
+//        imgV.backgroundColor = .lightGray
         imgV.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(imgV)
         let imgVleading = NSLayoutConstraint(item: imgV, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 10)
-        let imgVtop = NSLayoutConstraint(item: imgV, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: 10);
+        let imgVtop = NSLayoutConstraint(item: imgV, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: UIApplication.shared.statusBarFrame.height + 50);
         let imgVtrailing = NSLayoutConstraint(item: imgV, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: -10);
         let imgVheight = NSLayoutConstraint(item: imgV, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: self.view.bounds.size.height * 0.5);
         NSLayoutConstraint.activate([imgVleading,imgVtop,imgVtrailing,imgVheight])
@@ -91,6 +101,90 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
         let cameraBtnBottom = NSLayoutConstraint(item: cameraBtn, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: -10);
         let cameraBtnHeight = NSLayoutConstraint(item: cameraBtn, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1.0, constant: self.view.bounds.size.height * 0.1);
         NSLayoutConstraint.activate([cameraBtnleading,cameraBtntrailing,cameraBtnBottom,cameraBtnHeight])
+        if languageSeg == nil{
+            languageSeg = UISegmentedControl(items: ["中文","梵文","日文","韓文"])
+            languageSeg.frame = .zero
+            languageSeg.selectedSegmentIndex = 0
+            languageSeg.selectedSegmentTintColor = .red
+            languageSeg.tintColor = .white
+            languageSeg.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(languageSeg)
+            
+            let segleading = NSLayoutConstraint(item: languageSeg, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 10)
+            let segtrailing = NSLayoutConstraint(item: languageSeg, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: -10);
+            let segTop = NSLayoutConstraint(item: languageSeg, attribute: .top, relatedBy: .equal, toItem: self.view, attribute: .top, multiplier: 1.0, constant: UIApplication.shared.statusBarFrame.height + 5)
+            let segBottom = NSLayoutConstraint(item: languageSeg, attribute: .bottom, relatedBy: .equal, toItem: self.imgV, attribute: .top, multiplier: 1.0, constant: -5);
+            NSLayoutConstraint.activate([segleading,segtrailing,segTop,segBottom])
+        }
+        
+    }
+    func textML(image:UIImage){
+        // When using Latin script recognition SDK
+        let latinOptions = TextRecognizerOptions()
+        let latinTextRecognizer = TextRecognizer.textRecognizer(options:latinOptions)
+
+        // When using Chinese script recognition SDK
+        let chineseOptions = ChineseTextRecognizerOptions()
+        let chineseTextRecognizer = TextRecognizer.textRecognizer(options:chineseOptions)
+
+        // When using Devanagari script recognition SDK
+        let devanagariOptions = DevanagariTextRecognizerOptions()
+        let devanagariTextRecognizer = TextRecognizer.textRecognizer(options:devanagariOptions)
+
+        // When using Japanese script recognition SDK
+        let japaneseOptions = JapaneseTextRecognizerOptions()
+        let japaneseTextRecognizer = TextRecognizer.textRecognizer(options:japaneseOptions)
+
+        // When using Korean script recognition SDK
+        let koreanOptions = KoreanTextRecognizerOptions()
+        let koreanTextRecognizer = TextRecognizer.textRecognizer(options:koreanOptions)
+        
+        let visionImage = VisionImage(image: image)
+        var textRecognizer:TextRecognizer!
+        switch self.languageSeg.selectedSegmentIndex{
+        case 0:
+            let chineseOptions = ChineseTextRecognizerOptions()
+            textRecognizer = TextRecognizer.textRecognizer(options:chineseOptions)
+            break
+        case 1:
+            let devanagariOptions = DevanagariTextRecognizerOptions()
+            textRecognizer = TextRecognizer.textRecognizer(options:devanagariOptions)
+            break
+        case 2:
+            let japaneseOptions = JapaneseTextRecognizerOptions()
+            textRecognizer = TextRecognizer.textRecognizer(options:japaneseOptions)
+            break
+        case 3:
+            let koreanOptions = DevanagariTextRecognizerOptions()
+            textRecognizer = TextRecognizer.textRecognizer(options:koreanOptions)
+            break
+        default:
+            let chineseOptions = ChineseTextRecognizerOptions()
+            textRecognizer = TextRecognizer.textRecognizer(options:chineseOptions)
+            break
+        }
+        textRecognizer.process(visionImage) {
+            result, error in
+            if error == nil{
+                let resultText = result!.text
+                self.textView.text += "\n圖片文字識別: "
+                print("讀取到的總訊息:\(resultText)")
+                if let resultBlocks = result?.blocks{
+                    self.parseORCText(resultBlocks: resultBlocks)
+                }
+            }
+        }
+    }
+    func parseORCText(resultBlocks:[TextBlock]){
+        for i in 0...resultBlocks.count - 1{
+            let block = resultBlocks[i]
+            let blockText = block.text
+            let blockLanguages = block.recognizedLanguages
+            let blockCornerPoints = block.cornerPoints
+            let blockFrame = block.frame
+            self.textView.text += "\n" + blockText
+            print("blockText:\(blockText)\nblockLanguages:\(blockLanguages)\nblockCornerPoints:\(blockCornerPoints)\nblockFrame:\(blockFrame)")
+        }
     }
     //MARK: UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -99,6 +193,7 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         print("didFinishPickingMediaWithInfo:\(info)")
+        self.textView.text = ""
         picker.dismiss(animated: true)
         let img = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerOriginalImage")] as! UIImage
         self.imgV.image = img
@@ -108,6 +203,26 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
         } catch {
             print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
         }
+        /*firebase image ML
+         let FBimage = VisionImage(image: img)
+ //        visionImage.orientation = image.imageOrientation
+         let options = ImageLabelerOptions()
+         options.confidenceThreshold = 0.7
+         let labeler = ImageLabeler.imageLabeler(options: options)
+         labeler.process(FBimage) {
+             labels, error in
+             if error == nil{
+                 for i in 0 ... labels!.count - 1{
+                     let label = labels![i]
+                     let labelText = label.text
+                     let confidence = label.confidence
+                     let index = label.index
+                     print("index:\(index)\nlabel:\(label)\nlabelText:\(labelText)\nconfidence:\(confidence)")
+                 }
+             }
+         }
+         */
+        self.textML(image: img)
     }
 
     
@@ -117,7 +232,7 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
 
         let predictionString = formattedPredictions.joined(separator: "\n")
         
-        self.textView.text = predictionString
+        self.textView.text += "圖片識別: " + predictionString
     }
     
     private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
